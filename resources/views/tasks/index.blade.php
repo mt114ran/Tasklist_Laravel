a313bff2568dc07c7131691348bd1aa52649757a
tomoya.maita <mt114_r8an@yahoo.co.jp>
2019-12-26 13:54:46 +0000

    login show

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -19,11 +19,13 @@
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
+                @if (\Auth::id() === $task->user_id)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->status}}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
+                @endif
                 @endforeach
             </tbody>
         </table>
